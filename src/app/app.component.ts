9ba5e928ca731530e05386f400198e495d9a3db9
김성현 <921126sh@thewc.co.kr>
2021-05-27 15:52:21 +0900

    오타수정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, KeyValueDiffer, KeyValueDiffers, OnInit } from '@angular/core';
 import { NgxSpinnerService } from "ngx-spinner";
 import { finalize } from 'rxjs/operators';
 
@@ -19,8 +19,119 @@ interface ResData {
 })
 export class AppComponent implements OnInit {
     title = 'smart-store';
+
+
     selected = 1;
+
     isCheckReceiverAddressChanged = true;
+
+    // afterClaimStatus: null
+    // afterClaimType: null
+    // afterProductOrderStatus: null
+    // beforeClaimStatus: null
+    // beforeClaimType: null
+    // beforeProductOrderStatus: null
+    // errorCode: "ERR-NC-UNKNOWN"
+    // errorDetail: "Transaction ID: 52CBDCD22D9A4B6CA020296096810A60105A"
+    // errorMessage: "상품주문 정보 조회실패(일시적인 장애)"
+    // mallId: null
+    // parameterJson: "{\"ProductOrderID\": \"\"}"
+    // productId: null
+    // productName: null
+    // productOption: null
+    // productOrderId: null
+
+    settings = {
+        pager: {
+            display: false
+        },
+        actions: {
+            add: false,
+            edit: false,
+            delete: false,
+        },
+        columns: {
+            productOrderId: {
+                title: '상품ID'
+            },
+            productName: {
+                title: '상품명'
+            },
+            beforeProductOrderStatus: {
+                title: '변경전 주문상태'
+            },
+            beforeClaimStatus: {
+                title: '변경전 클레임 상태'
+            },
+            beforeClaimType: {
+                title: '변경전 클레임 유형'
+            },
+            // apiName: {
+            //     title: '요청 api'
+            // },
+            parameterJson: {
+                title: '요청 파라미터'
+            },
+            afterProductOrderStatus: {
+                title: '변경후 주문상태'
+            },
+            afterClaimStatus: {
+                title: '변경후 클레임 상태'
+            },
+            afterClaimType: {
+                title: '변경후 클레임 유형'
+            },
+        }
+    };
+
+    settings2 = {
+        pager: {
+            display: false
+        },
+        actions: {
+            add: false,
+            edit: false,
+            delete: false,
+        },
+        columns: {
+            productOrderId: {
+                title: '상품ID'
+            },
+            productName: {
+                title: '상품명'
+            },
+            beforeProductOrderStatus: {
+                title: '변경전 주문상태'
+            },
+            beforeClaimStatus: {
+                title: '변경전 클레임 상태'
+            },
+            beforeClaimType: {
+                title: '변경전 클레임 유형'
+            },
+            // apiName: {
+            //     title: '요청 api'
+            // },
+            parameterJson: {
+                title: '요청 파라미터'
+            },
+            afterProductOrderStatus: {
+                title: '변경후 주문상태'
+            },
+            afterClaimStatus: {
+                title: '변경후 클레임 상태'
+            },
+            afterClaimType: {
+                title: '변경후 클레임 유형'
+            },
+            errorCode: {
+                title: '에러코드'
+            },
+            errorMessage: {
+                title: '에러메시지'
+            }
+        }
+    };
 
     reqData: ReqData = {
         'apiName': "",
@@ -29,16 +140,35 @@ export class AppComponent implements OnInit {
 
     resData: ResData = {};
 
+    selectedDiffer: KeyValueDiffer<string, any>;
+
     /**
      * endpoint
      */
     readonly URL = 'http://localhost:8080/';
 
     constructor(private http: HttpClient,
-        private spinner: NgxSpinnerService) {
+        private spinner: NgxSpinnerService,
+        private differs: KeyValueDiffers) {
+
+        this.selectedDiffer = this.differs.find({ "tt": this.selected }).create();
     }
 
     ngOnInit(): void {
+
+    }
+    ngOnChanges() {
+        console.log("CHANGES")
+    }
+    ngDoCheck(): void {
+        const changes = this.selectedDiffer.diff({ "tt": this.selected });
+
+        if (changes) {
+            this.init();
+        }
+    }
+
+    init(): void {
         this.reqData = {
             'apiName': "",
             'params': {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,23 @@ import { FormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from "ngx-spinner";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    NgxSpinnerModule,
-    BrowserAnimationsModule,
-    NgxJsonViewerModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        NgxSpinnerModule,
+        BrowserAnimationsModule,
+        NgxJsonViewerModule,
+        Ng2SmartTableModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
